d6cfcb273711769db0c320d5ba77dfaeeae2d4a6
Ferris Klingenberg <ferris.klingenberg@gmail.com>
2022-10-22 00:31:45 +0200

    Initial commit

---
+++ src/hooks/useTranslation.ts
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+
+export const useTranslation = (locale?: string) => {
+    const router = useRouter();
+
+    return (
+        key: string,
+        variables?: { [variable: string]: string | number }
+    ) => {
+        let data = require(`./public/locales/${locale ?? router.locale}.json`);
+        const keyPart = key.split(".");
+
+        for (let i = 0; i < keyPart.length; i++) {
+            data = data[keyPart[i]];
+        }
+
+        let translation: string = data ?? key;
+
+        for (const [variable, value] of Object.entries(variables ?? {})) {
+            translation = translation.replace(`{{${variable}}}`, String(value));
+        }
+
+        return translation;
+    };
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { useTranslation } from "./hooks/useTranslation";
+export { useTranslation };
